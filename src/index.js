a801b67713fb74ba79bffa27f3dc83369374e3ba
Cryptk1ddo <91121293+Cryptk1ddo@users.noreply.github.com>
2025-08-21 23:24:59 +0000

    Replace Tailwind with Chakra UI, remove PostCSS/tailwind config

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
