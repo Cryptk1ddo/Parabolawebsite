da92a585f44a00c6d7d8c8cb2fbf8526d1eb420d
Cryptk1ddo <91121293+Cryptk1ddo@users.noreply.github.com>
2025-08-21 23:54:12 +0000

    small changes to remove unused files

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-
-const theme = extendTheme({});
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider theme={theme}>
+  <ChakraProvider>
     <App />
   </ChakraProvider>
 );
